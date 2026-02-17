be7553889b3e838591352ba9c1d51b6b6181e47f
Aws12505 <awsaljaramani12505@gmail.com>
2026-02-17 12:24:43 -0500

    ..

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,14 @@ use App\Http\Middleware\AuthTokenStoreScopeMiddleware;
 Route::middleware([
     AuthTokenStoreScopeMiddleware::class,
 ])->group(function () {
-    
+
+    Route::get('/audits/ratings-summary/{store_id}/{date_start}/{date_end}', [AuditController::class, 'ratingsSummary']);
 
     Route::apiResource('camera-forms', CameraFormController::class)
-    ->except(['update']);  // Exclude the default `update` route
+        ->except(['update']);  // Exclude the default `update` route
 
-// Define custom `update` route with POST method
-Route::post('camera-forms/{camera_form}', [CameraFormController::class, 'update']);
+    // Define custom `update` route with POST method
+    Route::post('camera-forms/{camera_form}', [CameraFormController::class, 'update']);
 
     Route::get('camera-reports', [CameraReportController::class, 'index']);
     Route::get('camera-reports/export', [CameraReportController::class, 'export']);
